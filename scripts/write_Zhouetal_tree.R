34c49227c26b8ed075d92883eddd61431dc46255
KatieFish <kjf214@lehigh.edu>
2020-03-24 11:27:43 -0500

    added RAxML script

---
+++ scripts/write_Zhouetal_tree.R
@@ -0,0 +1,11 @@
+
+write_Zhouetal_tree<-function(bootstraps) {
+require("seqinr")
+require("ape")
+require("ips")
+bootstraps->n
+alnmnt<-read.alignment("~/UG_research_COVID_module/alnmnts/betacoronaviruses.aln", format="clustal")
+alnmnt<-as.DNAbin(alnmnt)
+raxml(alnmnt, m="GTRCAT", N=n, p=5, f="a", x=2, k=FALSE, exec="~/standard-RAxML/raxmlHPC-SSE3", backbone=NULL)
+
+}
