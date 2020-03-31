1d42b17810bcf34483ce26f53fdf3d5d95c5fb38
KatieFish <kjf214@lehigh.edu>
2020-03-31 12:28:48 -0500

    added mafft betacoronavirus alignment

--- scripts/write_Zhouetal_tree.R
+++ scripts/write_Zhouetal_tree.R
@@ -4,7 +4,7 @@ require("seqinr")
 require("ape")
 require("ips")
 bootstraps->n
-alnmnt<-read.alignment("~/Desktop/UG_research_COVID_module/alnmnts/betacoronaviruses.aln", format="clustal")
+alnmnt<-read.alignment("~/Desktop/UG_research_COVID_module/alnmnts/Beta_alpha_coronavirus.aln", format="fasta")
 alnmnt<-as.DNAbin(alnmnt)
 raxml(alnmnt, m="GTRCAT", N=n, p=5, f="a", x=2, k=FALSE, exec="~/standard-RAxML/raxmlHPC-SSE3", backbone=NULL)
 
